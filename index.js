c73f8738354d78753094cd70f110ab127877dd0c
2jj6 <apurposetoall@gmail.com>
2025-07-01 00:13:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Entry point for the app
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
